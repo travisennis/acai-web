44d8a99e35cbc82fcb9bb907de6cbbfa5e76a5a1
Travis Ennis <travis@travisennis.com>
2025-01-04 22:27:53 -0500

    refactor: moved commands/processPrompt to separate file

---
+++ source/commands.ts
@@ -0,0 +1,102 @@
+import fs from "node:fs/promises";
+import { existsSync } from "node:fs";
+import path from "node:path";
+
+const BODY_CONTENTS = /<body[^>]*>([\s\S]*?)<\/body>/i;
+export async function processPrompt(
+  message: string,
+  { baseDir }: { baseDir: string },
+) {
+  const lines = message.split("\n");
+  const processedLines: string[] = [];
+  const attachments: Buffer[] = [];
+  let projectDir: string | null = null;
+  let fileDirectiveFound = false;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.startsWith("@projectdir")) {
+      const project = line.replace("@projectdir ", "").trim();
+
+      projectDir = path.join(baseDir, project);
+      if (!existsSync(projectDir)) {
+        throw new Error(`${projectDir} does not exist.`);
+      }
+    } else if (line.startsWith("@list-prompts")) {
+      fileDirectiveFound = true;
+
+      const dirs = await fs.readdir(`${baseDir}/prompts`);
+
+      processedLines.push(`Prompts:\n${dirs.join("\n")}\n`);
+    } else if (line.startsWith("@prompt ")) {
+      fileDirectiveFound = true;
+      const promptName = line.replace("@prompt ", "").trim();
+
+      const f = await fs.readFile(
+        `${baseDir}/prompts/${promptName}.md`,
+        "utf8",
+      );
+
+      processedLines.push(`${f}`);
+    } else if (line.startsWith("@file ")) {
+      fileDirectiveFound = true;
+      const filePath = line.replace("@file ", "").trim();
+      const fileExtension = filePath.split(".").pop();
+
+      const f = await fs.readFile(`${baseDir}${filePath}`.trim(), "utf8");
+
+      processedLines.push(
+        `File: ${filePath}\n\`\`\` ${fileExtension}\n${f}\n\`\`\``,
+      );
+    } else if (line.startsWith("@files ")) {
+      fileDirectiveFound = true;
+      const filePaths = line.replace("@files ", "");
+      for (const filePath of filePaths.split(" ")) {
+        const fileExtension = filePath.split(".").pop();
+        const f = await fs.readFile(`${baseDir}${filePath}`.trim(), "utf8");
+
+        processedLines.push(
+          `File: ${filePath}\n\`\`\` ${fileExtension}\n${f}\n\`\`\``,
+        );
+      }
+    } else if (line.startsWith("@dir")) {
+      fileDirectiveFound = true;
+      const filePath = line.replace("@dir", "").trim();
+
+      const dirs = await fs.readdir(`${baseDir}${filePath}`);
+
+      processedLines.push(`File tree:\n${dirs.join("\n")}\n`);
+    } else if (line.startsWith("@url ")) {
+      fileDirectiveFound = true;
+      const urlPath = line.replace("@url ", "").trim();
+      const response = await fetch(urlPath);
+      if (!response.ok) {
+        processedLines.push(`Url:${urlPath}\nStatus: ${response.status}`);
+      } else {
+        const text = await response.text();
+        const processedText = (text.match(BODY_CONTENTS)?.[1] || "").replace(
+          /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+          "",
+        );
+        processedLines.push(
+          `URL: ${urlPath}\n\`\`\`\n${processedText.trim()}\n\`\`\`\n`,
+        );
+      }
+    } else if (line.startsWith("@pdf ")) {
+      const pdfUrl = line.replace("@pdf ", "");
+
+      const response = await fetch(pdfUrl);
+      const buffer = await response.arrayBuffer();
+      const pdfBuffer = Buffer.from(buffer);
+      attachments.push(pdfBuffer);
+    } else {
+      processedLines.push(line);
+    }
+  }
+
+  return {
+    processedPrompt: processedLines.join("\n").trim(),
+    attachments,
+    returnPrompt: fileDirectiveFound,
+    projectDir,
+  };
+}
--- source/instruct.ts
+++ source/instruct.ts
@@ -1,5 +1,3 @@
-import { existsSync } from "node:fs";
-import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { zValidator } from "@hono/zod-validator";
@@ -22,6 +20,7 @@ import {
 import { type CoreMessage, type UserContent, generateText } from "ai";
 import { Hono } from "hono";
 import { z } from "zod";
+import { processPrompt } from "./commands.ts";
 
 export const app = new Hono().post(
   "/",
@@ -126,8 +125,6 @@ export const app = new Hono().post(
       ...thinkingTools,
     };
 
-    console.log(Object.keys(allTools).join(", "));
-
     const { text } = await generateText({
       model: wrapLanguageModel(
         languageModel(chosenModel),
@@ -158,105 +155,6 @@ export const app = new Hono().post(
 
 function objectKeys<T extends object>(obj: T): Array<keyof T> {
   return Object.keys(obj) as Array<keyof T>;
-}
-
-const BODY_CONTENTS = /<body[^>]*>([\s\S]*?)<\/body>/i;
-async function processPrompt(
-  message: string,
-  { baseDir }: { baseDir: string },
-) {
-  const lines = message.split("\n");
-  const processedLines: string[] = [];
-  const attachments: Buffer[] = [];
-  let projectDir: string | null = null;
-  let fileDirectiveFound = false;
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    if (line.startsWith("@projectdir")) {
-      const project = line.replace("@projectdir ", "").trim();
-
-      projectDir = path.join(baseDir, project);
-      if (!existsSync(projectDir)) {
-        throw new Error(`${projectDir} does not exist.`);
-      }
-    } else if (line.startsWith("@list-prompts")) {
-      fileDirectiveFound = true;
-
-      const dirs = await fs.readdir(`${baseDir}/prompts`);
-
-      processedLines.push(`Prompts:\n${dirs.join("\n")}\n`);
-    } else if (line.startsWith("@prompt ")) {
-      fileDirectiveFound = true;
-      const promptName = line.replace("@prompt ", "").trim();
-
-      const f = await fs.readFile(
-        `${baseDir}/prompts/${promptName}.md`,
-        "utf8",
-      );
-
-      processedLines.push(`${f}`);
-    } else if (line.startsWith("@file ")) {
-      fileDirectiveFound = true;
-      const filePath = line.replace("@file ", "").trim();
-      const fileExtension = filePath.split(".").pop();
-
-      const f = await fs.readFile(`${baseDir}${filePath}`.trim(), "utf8");
-
-      processedLines.push(
-        `File: ${filePath}\n\`\`\` ${fileExtension}\n${f}\n\`\`\``,
-      );
-    } else if (line.startsWith("@files ")) {
-      fileDirectiveFound = true;
-      const filePaths = line.replace("@files ", "");
-      for (const filePath of filePaths.split(" ")) {
-        const fileExtension = filePath.split(".").pop();
-        const f = await fs.readFile(`${baseDir}${filePath}`.trim(), "utf8");
-
-        processedLines.push(
-          `File: ${filePath}\n\`\`\` ${fileExtension}\n${f}\n\`\`\``,
-        );
-      }
-    } else if (line.startsWith("@dir")) {
-      fileDirectiveFound = true;
-      const filePath = line.replace("@dir", "").trim();
-
-      const dirs = await fs.readdir(`${baseDir}${filePath}`);
-
-      processedLines.push(`File tree:\n${dirs.join("\n")}\n`);
-    } else if (line.startsWith("@url ")) {
-      fileDirectiveFound = true;
-      const urlPath = line.replace("@url ", "").trim();
-      const response = await fetch(urlPath);
-      if (!response.ok) {
-        processedLines.push(`Url:${urlPath}\nStatus: ${response.status}`);
-      } else {
-        const text = await response.text();
-        const processedText = (text.match(BODY_CONTENTS)?.[1] || "").replace(
-          /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
-          "",
-        );
-        processedLines.push(
-          `URL: ${urlPath}\n\`\`\`\n${processedText.trim()}\n\`\`\`\n`,
-        );
-      }
-    } else if (line.startsWith("@pdf ")) {
-      const pdfUrl = line.replace("@pdf ", "");
-
-      const response = await fetch(pdfUrl);
-      const buffer = await response.arrayBuffer();
-      const pdfBuffer = Buffer.from(buffer);
-      attachments.push(pdfBuffer);
-    } else {
-      processedLines.push(line);
-    }
-  }
-
-  return {
-    processedPrompt: processedLines.join("\n").trim(),
-    attachments,
-    returnPrompt: fileDirectiveFound,
-    projectDir,
-  };
 }
 
 export default app;
